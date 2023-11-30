1eda6bcfa7ccc9f07c5e5a07f4e6fe3f382501a8
SangDoo Nam <kexe009@hotmail.com>
2023-11-30 14:10:06 +0100

    modified

--- src/ex.py
+++ src/ex.py
@@ -19,30 +19,31 @@
 
 
 #Task 3
-with open(file='data/task3.txt', mode='r') as task3:
-    content = task3.readlines()
-    odd = [content[i] for i in range(len(content)) if bool((i+1)%2)]
-    even = [content[i] for i in range(len(content)) if not bool((i+1)%2)]
-    combined = odd + even
-    result = ''.join(combined)
-    print(result)
+# with open(file='data/task3.txt', mode='r') as task3:
+#     content = task3.readlines()
+#     odd = [content[i] for i in range(len(content)) if bool((i+1)%2)]
+#     even = [content[i] for i in range(len(content)) if not bool((i+1)%2)]
+#     combined = odd + even
+#     result = ''.join(combined)
+#     print(result)
 
 
 #Task 4
 
 # with open(file='data/task4.txt', mode='r') as task4:
-#     content = task4.readlines()
-#     txt = ''.join(content)
-#     print(txt[1622:1634+1])
+#     print(task4.read()[1622:1634+1])
+    # content = task4.readlines()
+    # txt = ''.join(content)
+    # print(txt[1622:1634+1])
 
 
 #Task 5
 
 # with open(file='data/task5.txt', mode='r') as task5:
-#     content = task5.readline(70)
+#     content = task5.readline()
 #     pointer = task5.tell()
 #     print(content)
-#     print(pointer)
+#     print(pointer-1)
 
 
 
