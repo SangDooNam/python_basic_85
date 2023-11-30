8daceec0300c8160bdd0aa4fdd6a4844b907b120
SangDoo Nam <kexe009@hotmail.com>
2023-11-30 13:50:04 +0100

    modified

--- src/ex.py
+++ src/ex.py
@@ -19,13 +19,13 @@
 
 
 #Task 3
-# with open(file='data/task3.txt', mode='r') as task3:
-#     content = task3.readlines()
-#     odd = [content[i] for i in range(len(content)) if not bool(i%2)]
-#     even = [content[i] for i in range(len(content)) if bool(i%2)]
-#     combined = odd + even
-#     result = ''.join(combined)
-#     print(result)
+with open(file='data/task3.txt', mode='r') as task3:
+    content = task3.readlines()
+    odd = [content[i] for i in range( len(content)) if bool(i%2)]
+    even = [content[i] for i in range( len(content)) if not bool(i%2)]
+    combined = even + odd
+    result = ''.join(combined)
+    print(result)
 
 
 #Task 4
